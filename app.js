dde82c13092eea69bb7cd8a6d73e3a4cde4a40ea
Sara <sm7661110@gmail.com>
2024-11-05 16:30:45 +0300

    creating hello message

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-alert("Hello ggg hhhh"); 
- 
+<script>
+            let weather = prompt(" the weather (sunny, rainy, or other):");
+
+        if (weather == "sunny") {
+         document.write("It's a beautiful day!");
+      
+        } else if (weather == "rainy") {
+       document.write("Don't forget your umbrella!");
+      
+    } else {
+      document.write("Check the forecast before going out.");
+}
+
+</script>
